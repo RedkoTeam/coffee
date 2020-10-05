b677ad8c290ac5df4431d80e2bcc80cb6ef69b3a
docholliday211 <joshellison.redko@gmail.com>
2020-10-05 15:52:09 -0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import reading from './assets/reading.png';
+import sharePhoto from './assets/sharePhoto.png';
+import largeTitle from './assets/largeTitle.png';
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={largeTitle} style={styles.appLogo} />
+      <View style={styles.logoContainer}>
+        <Image source={reading} style={styles.logo} />
+        <Image source={sharePhoto} style={styles.logo} />
+      </View>
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +43,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E5E5E5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logoContainer: {
+    flexDirection: "row",
+    padding: 20,
+  },
+  logo: {
+    width: 160,
+    height: 130,
+    marginHorizontal: 10,
+  },
+  appLogo: {
+    width: 320,
+    height: 38,
+    paddingBottom: 30
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  }, 
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
+
 });
+
