6f546c94925f3e72029a2aec92ced3b9e0405a6d
docholliday211 <joshellison.redko@gmail.com>
2020-10-28 14:24:52 -0700

    home page done

--- App.js
+++ App.js
@@ -17,12 +17,13 @@ import SignInButton from './assets/FortuneCoffeePNGassets/SignInButton.png';
 import SignUpButton from './assets/FortuneCoffeePNGassets/SignUpButton.png';
 import LargeTitleApp from './assets/FortuneCoffeePNGassets/LargeTitleApp.png';
 import PickCard from './assets/FortuneCoffeePNGassets/PickCard.png';
-import RightCard from './assets/FortuneCoffeePNGassets/RightCard.png';
-import MiddleCard from './assets/FortuneCoffeePNGassets/MiddleCard.png';
-import LeftCard from './assets/FortuneCoffeePNGassets/LeftCard.png';
-import Love from './assets/FortuneCoffeePNGassets/Love.png';
-import Work from './assets/FortuneCoffeePNGassets/Work.png';
-import Wish from './assets/FortuneCoffeePNGassets/Wish.png';
+// import RightCard from './assets/FortuneCoffeePNGassets/RightCard.png';
+// import MiddleCard from './assets/FortuneCoffeePNGassets/MiddleCard.png';
+// import LeftCard from './assets/FortuneCoffeePNGassets/LeftCard.png';
+// import Love from './assets/FortuneCoffeePNGassets/Love.png';
+// import Work from './assets/FortuneCoffeePNGassets/Work.png';
+// import Wish from './assets/FortuneCoffeePNGassets/Wish.png';
+import Cards from './assets/FortuneCoffeePNGassets/allCards.png';
 import Ellipse1 from './assets/FortuneCoffeePNGassets/Ellipse6.png';
 import Ellipse2 from './assets/FortuneCoffeePNGassets/Ellipse7.png';
 
@@ -42,25 +43,25 @@ const styles = StyleSheet.create({
   },
   authButton1: {
     right: 90,
-    top: 50
+    top: 30
   },
   authButton2: {
     left: 90,
-    top: 50,
+    top: 30,
   },
   appTitle: {
-    paddingBottom: 10,
-    paddingTop: 20,
+    paddingBottom: 30,
+    paddingTop: 30,
   },
   cardTitle: {
     paddingTop: 15,
-    paddingBottom: 5
   },
   circleContainer: {
     flex: .5,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingBottom: 10,
   },
   circleL: {
     left: 145
@@ -68,56 +69,10 @@ const styles = StyleSheet.create({
   circleR: {
     right: 145
   },
-  cardContainer: {
-    flex: 1,
-    zIndex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+  cards: {
+    paddingTop: 10,
+    bottom: -20,
   },
-  cardTitleContainer: {
-    flex: .01,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  cardL: {
-    position: 'absolute',
-    flex: 1,
-    left: -200,
-    bottom: -150,
-  },
-  cardM: {
-    position: 'absolute',
-    left: -110,
-    bottom: -150,
-  },
-  cardR: {
-    position: 'absolute',
-    bottom: -150,
-    right: -200,
-  },
-  love: {
-    position: 'absolute',
-    bottom: -65,
-    left: -160
-  },
-  wish: {
-    position: 'absolute',
-    bottom: -35,
-    left: -25
-  },
-  work: {
-    position: 'absolute',
-    bottom: -65,
-    right: -160
-  },
-  // iconContainer: {
-  //   flex: .5,
-  //   flexDirection: 'row',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
   ellipse1: {
     position: 'absolute',
     bottom: -0,
@@ -129,15 +84,15 @@ const styles = StyleSheet.create({
     left: 0
   },
   favIcon: {
-    bottom: -100,
+    bottom: -124,
     left: -130
   },
   homeIcon: {
-    bottom: 30,
+    bottom: -10,
     left: -3
   },
   shopIcon: {
-    bottom: 15,
+    bottom: -10,
     right: -130
   },
   
@@ -177,51 +132,13 @@ function HomeScreen({navigation}) {
       <View style={styles.cardTitle}>
         <Image source={PickCard} />
       </View>
-      <View style={styles.cardTitleContainer}>
-        <TouchableOpacity onPress={() => navigation.navigate('Virtual')}>
-          <Image
-            source={Love}
-            style={styles.love}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Virtual')}>
-          <Image
-            source={Work}
-            style={styles.work}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Virtual')}>
-          <Image
-            source={Wish}
-            style={styles.wish}
-          />
-        </TouchableOpacity>
-      </View>
+      <TouchableOpacity onPress={() => navigation.navigate('Virtual')} style={styles.cards}>
+        <Image source={Cards} />
+      </TouchableOpacity>
 
-      {/* TODO: onPress currently not working on anyting with position absolute. Find fix/workaround */}
-      <View style={styles.cardContainer} >
-        <TouchableOpacity onPress={() => navigation.navigate('Virtual')}>
-          <Image 
-            source={LeftCard}
-            style={styles.cardL}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('PhotoReading')}>
-          <Image
-            source={RightCard}
-            style={styles.cardR}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('PhotoReading')}>
-          <Image 
-            source={MiddleCard}
-            style={styles.cardM}
-          />
-        </TouchableOpacity>
-      </View>
         <Image source={Ellipse1} style={styles.ellipse1} />
         <Image source={Ellipse2} style={styles.ellipse2} />
-      {/* <View style={styles.iconContainer}> */}
+
       <TouchableOpacity onPress={() => navigation.navigate('Favorites')} style={styles.favIcon}>
           <Image source={Favorites} onPress={() => navigation.navigate('Favorites')}/>
         </TouchableOpacity>
@@ -231,7 +148,7 @@ function HomeScreen({navigation}) {
       <TouchableOpacity onPress={() => navigation.navigate('Shop')} style={styles.shopIcon}>
           <Image source={Shop} />
         </TouchableOpacity>
-      {/* </View> */}
+      
   
     </View>
   );
